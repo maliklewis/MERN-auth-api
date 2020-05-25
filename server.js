f148b192bdbd29d07bb0c259a5afcd213ed7aa30
Malik lewis <maliklewis@cmail.carleton.ca>
2020-05-24 22:21:05 -0400

    prod changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const bodyParser = require('body-parser'); //reading api response data
+const path = require("path");
 
 //db connection
 mongoose.connect('mongodb://127.0.0.1:27017/mernauth', {
@@ -26,9 +27,22 @@ const userRoutes = require('./routes/user');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 //app.use(cors()); //overrides cors errors in the browers *thank god*
-if (process.env.NODE_ENV = 'dev') {
+if (process.env.NODE_ENV = 'development') {
     app.use(cors({origin: `http://localhost:3000`})); //react front end port to allow to connect to port 8000
 }
+
+//React middleware
+//app.use(compression());
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+ 
+    app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+}
+
+
+
 
 //middleware
 app.use('/api/', authRoutes);
