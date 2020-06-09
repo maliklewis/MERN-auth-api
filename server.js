92f2c2ad1002b19121953215be6a2ff670d8f389
Malik lewis <maliklewis@cmail.carleton.ca>
2020-06-08 20:45:53 -0400

    CORS fixes

--- server.js
+++ server.js
@@ -26,15 +26,15 @@ const userRoutes = require('./routes/user');
 //app middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cors()); //overrides cors errors in the browers *thank god*
-// if (process.env.NODE_ENV = 'development') {
-//     app.use(cors({origin: `http://localhost:3000`})); //react front end port to allow to connect to port 8000
-// }
+//app.use(cors()); //overrides cors errors in the browers *thank god*
+if (process.env.NODE_ENV = 'development') {
+    app.use(cors({origin: `http://localhost:3000`})); //react front end port to allow to connect to port 8000
+}
 
 //React middleware
 //app.use(compression());
 if (process.env.NODE_ENV === 'production') {
-    // app.use(cors({origin: `http://www.maliklewis.ca`}));
+    app.use(cors({origin: `http://www.maliklewis.ca`}));
     app.use(express.static(path.join(__dirname, 'build')));
  
     app.get('*', function(req, res) {
