26af1fa0ca2c0ee21c9bdfecc0ff3e385d39744f
Malik lewis <maliklewis@cmail.carleton.ca>
2020-06-14 16:19:36 -0400

    Forgot/reset password implementation

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,22 @@ const {
     signup,
     accountActivation,
     signin,
-    googleLogin
+    googleLogin,
+    forgotPassword,
+    resetPassword
 } = require('../controllers/auth');
 
 //import validator
-const {userSignupValidator, userSigninValidator} = require('../validators/auth')
+const {userSignupValidator, userSigninValidator, forgotPasswordValidator, resetPasswordValidator} = require('../validators/auth')
 const {runValidation} = require('../validators') //if the file is names index.js you dont need to write it
 
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/account-activation', accountActivation);
 router.post('/signin', userSigninValidator, runValidation, signin);
+
+//reset password
+router.put('/forgot-password',forgotPasswordValidator, runValidation, forgotPassword);
+router.put('/reset-password',resetPasswordValidator, runValidation, resetPassword);
 
 //google and facebook
 router.post('/google-login', googleLogin)
