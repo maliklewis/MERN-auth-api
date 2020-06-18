1abd609ba26b4b0f5992a9419fda22b34f2c62a7
Malik Lewis <maliklewis@MacBook-Pro.local>
2020-06-18 18:42:05 -0400

    production fix via hashroute

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,7 @@ exports.signup = (req, res) => {
             subject:`Account activation link`,
             html: `
                 <h2>Please use the following link to reset your password: </h2>
-                <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+                <p>${process.env.CLIENT_URL}/#/auth/activate/${token}</p>
                 <hr />
                 <p>This email may contain sensitive information, and is not monitored. Please do not reply to this email.</p>
                 <p>${process.env.CLIENT_URL}</p>
@@ -46,7 +46,7 @@ exports.signup = (req, res) => {
 };
 
 exports.accountActivation = (req, res) => {
-    const {token} = req.body
+    const {token} = req.body;
 
     if (token) {
         //token includes secret key, so checking if they match
